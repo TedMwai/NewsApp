56fc12a00bf1c5137e73db6786424a63570c07f9
Teddy Mwai <teddymwai11@gmail.com>
2023-05-16 03:02:27 +0300

    Added some UI logic

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.newsapp
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,8 +15,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.newsapp.ui.theme.NewsAppTheme
+import com.example.newsapp.utils.Constants.BUSINESS
+import com.example.newsapp.utils.Constants.ENTERTAINMENT
+import com.example.newsapp.utils.Constants.HEALTH
+import com.example.newsapp.utils.Constants.HOME
+import com.example.newsapp.utils.Constants.SCIENCE
+import com.example.newsapp.utils.Constants.SPORTS
+import com.example.newsapp.utils.Constants.TECHNOLOGY
 
 class MainActivity : ComponentActivity() {
+    // Tabs Title
+    private val newsCategories = arrayOf(
+        HOME, BUSINESS,
+        ENTERTAINMENT, SCIENCE,
+        SPORTS, TECHNOLOGY, HEALTH
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,6 +40,43 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    // Check internet connection
+    private fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        // For 29 api or above
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+                    ?: return false
+            return when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            // For below 29 api
+            if (connectivityManager.activeNetworkInfo != null && connectivityManager.activeNetworkInfo!!.isConnectedOrConnecting) {
+                return true
+            }
+        }
+        return false
+    }
+
+    companion object {
+        var generalNews: ArrayList<NewsModel> = ArrayList()
+        var entertainmentNews: MutableList<NewsModel> = mutableListOf()
+        var businessNews: MutableList<NewsModel> = mutableListOf()
+        var healthNews: MutableList<NewsModel> = mutableListOf()
+        var scienceNews: MutableList<NewsModel> = mutableListOf()
+        var sportsNews: MutableList<NewsModel> = mutableListOf()
+        var techNews: MutableList<NewsModel> = mutableListOf()
+        var apiRequestError = false
+        var errorMessage = "error"
     }
 }
 
